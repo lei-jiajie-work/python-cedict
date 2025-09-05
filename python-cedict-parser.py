fb730e6466e81c103c27e7e9d4dead4bdc3f45e8
Ishmael <jiajieleiwork52@gmail.com>
2025-09-05 02:08:08 +0000

    Add files via upload

---
+++ python-cedict-parser.py
@@ -0,0 +1,25 @@
+import io
+import re
+import json
+
+diction = open("cedict_ts.u8", "rt").read().split('\n')
+limit = 5
+
+with open('cedict-cfg.json', 'rt') as cfg:
+    cfg_data = json.load(cfg)
+    limit = cfg_data["limit"]
+    diction = open(cfg_data["dictionary_path"], "rt").read().split('\n')
+
+
+while True:
+    query = input("Type your search query or 'x' to cancel.\n")
+    if query == "x":
+        break
+    else:
+        count = 0
+        for line in diction:
+            if count == limit:
+                break
+            if query in line:
+                count += 1
+                print("\n" + line.strip("/"))
