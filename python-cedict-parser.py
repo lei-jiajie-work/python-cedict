171ce18c386c5f30325b01fb8ef7894cf75a9782
Ishmael <jiajieleiwork52@gmail.com>
2025-09-17 02:19:42 +0000

    Removed unneeded libraries
    removed io and re.

--- python-cedict-parser.py
+++ python-cedict-parser.py
@@ -1,5 +1,3 @@
-import io
-import re
 import json
 
 diction = open("cedict_ts.u8", "rt").read().split('\n')
@@ -9,7 +7,6 @@ with open('cedict-cfg.json', 'rt') as cfg:
     cfg_data = json.load(cfg)
     limit = cfg_data["limit"]
     diction = open(cfg_data["dictionary_path"], "rt").read().split('\n')
-
 
 while True:
     query = input("Type your search query or 'x' to cancel.\n")
